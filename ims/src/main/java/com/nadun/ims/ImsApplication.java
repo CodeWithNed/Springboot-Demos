59402b04af1c6b7c1047b1e554bf340f1199621e
CodeWithNed <nadunk.20@cse.mrt.ac.lk>
2024-12-22 13:17:22 +0530

    Init

---
+++ ims/src/main/java/com/nadun/ims/ImsApplication.java
@@ -0,0 +1,13 @@
+package com.nadun.ims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsApplication.class, args);
+	}
+
+}
